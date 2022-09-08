ea29ccfb82162ee6840f9534e4637bf0855bd7ae
Katlyn Boches <86095070+Katlyn627@users.noreply.github.com>
2022-09-08 09:03:57 -0600

    style bugs

--- App.js
+++ App.js
@@ -1,22 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import { s } from "react-native-wind";
-
+import React from 'react';
+import { Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={s`text-color-green`}>
-      <Text>Here is my Dating App!! Lets hope it works!!</Text>
+    <View className="flex-1 items-center justify-center bg-white">
+      <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
